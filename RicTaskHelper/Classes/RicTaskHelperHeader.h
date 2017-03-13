4e7a074379c5cc7f8ea3353cac2063f03f642c24
zhanglihuan <625482408@qq.com>
2017-03-13 23:45:19 +0800

    add a header file

---
+++ RicTaskHelper/Classes/RicTaskHelperHeader.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "RicDownloadTask.h"
+#import "RicTask.h"
+#import "RicTaskHelper.h"
+#import "RicUploadTask.h"
+
+FOUNDATION_EXPORT double RicTaskHelperVersionNumber;
+FOUNDATION_EXPORT const unsigned char RicTaskHelperVersionString[];
+
